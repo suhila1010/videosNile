8321e4bc7bc8ea88950834b513d81d8579a699be
suhila1010 <sohilaahmed678@gmail.com>
2020-05-15 15:03:28 +0200

    fهقسف

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#po").lightGallery();
+    $("#pt").lightGallery();
+    $("#ph").lightGallery();
+    $("#p4").lightGallery();
+    $("#p5").lightGallery();
+    $("#p6").lightGallery();
+    $("#p7").lightGallery();
+    $("#p8").lightGallery();
+    $("#p9").lightGallery();
+});
